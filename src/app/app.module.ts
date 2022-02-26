530ccb4b3b563b97d029396017d9068a87692038
hoatq-fptaptech <hoatq4@fpt.edu.vn>
2022-02-26 19:01:42 +0700

    assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {HeaderComponent} from "./header/header.component";
+import {FooterComponent} from "./footer/footer.component";
+import {LoginComponent} from "./login/login.component";
+import {RegisterComponent} from "./register/register.component";
+import {ForgotPasswordComponent} from "./forgotpassword/forgotpassword.component";
+import {RouterModule, Routes} from "@angular/router";
+import {FormsModule} from "@angular/forms";
 
+const appRoutes:Routes = [
+  {path:"",component:LoginComponent},
+  {path:"register",component:RegisterComponent},
+  {path:"forgot-password",component:ForgotPasswordComponent}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,HeaderComponent,FooterComponent,
+    LoginComponent,RegisterComponent,ForgotPasswordComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
